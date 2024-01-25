3b39c5d1b6b124d6f3322570fb223f39a91fc6db
martjn <mingdev77@gmail.com>
2024-01-25 15:50:08 +0200

    Created TodoController.getTodoById unit test and integration tests

--- tests/integration/todo.controller.int.test.js
+++ tests/integration/todo.controller.int.test.js
@@ -4,6 +4,8 @@ const newTodo = require("../mock-data/new-todo.json");
 
 const endpointUrl = "/todos/";
 
+let firstTodo;
+
 describe(endpointUrl, () => {
   it("POST" + endpointUrl, async () => {
     const response = await request(app).post(endpointUrl).send(newTodo);
@@ -24,11 +26,24 @@ describe(endpointUrl, () => {
       });
     }
   );
-  test("GET" + endpointUrl, async () => {
+  it("GET" + endpointUrl, async () => {
     const response = await request(app).get(endpointUrl);
     expect(response.statusCode).toBe(200);
     expect(Array.isArray(response.body)).toBeTruthy();
     expect(response.body[0].title).toBeDefined();
     expect(response.body[0].done).toBeDefined();
+    firstTodo = response.body[0];
+  });
+  it("GET by Id " + endpointUrl + ":todoId", async () => {
+    const response = await request(app).get(endpointUrl + firstTodo._id);
+    expect(response.statusCode).toBe(200);
+    expect(response.body.title).toBe(firstTodo.title);
+    expect(response.body.done).toBe(firstTodo.done);
+  });
+  it("GET todo by id doesn't exist" + endpointUrl + ":todoId", async () => {
+    const response = await request(app).get(
+      endpointUrl + "65b180ba9e0ca41bf698c1fa"
+    );
+    expect(response.statusCode).toBe(404);
   });
 });
